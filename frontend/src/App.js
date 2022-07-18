690f235ebd5e84e2d931d14b150eab0594ba3152
mansha126 <manshatiwari2612@gmail.com>
2022-07-18 12:38:39 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Home from './components/Home';
+import { BrowserRouter ,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
