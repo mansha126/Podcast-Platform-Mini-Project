9231bf4028d8823a766102442434645bddec7460
mansha126 <manshatiwari2612@gmail.com>
2022-07-28 13:41:09 +0530

    ...

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,18 +4,31 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg" style={{backgroundColor:"black"}}>
-        <div className="container-fluid" >
-          <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbEPrCQSxt3bwTSYQV0pglF1_rSzHIxR8aFab9uRdUInfa2gxVe1DHuuz0AEn3OWAOP8s&usqp=CAU" alt="" style={{ display:"block",
-        width: "70px",
-        
-            borderRadius: "100%"
-          }} />
-          <a className="navbar-brand" to="#" style={{ fontFamily: 'Satisfy', color: 'white' }}>
+      <nav
+        className="navbar navbar-expand-lg"
+        style={{ backgroundColor: "black" }}
+      >
+        <div className="container-fluid">
+          <img
+            className="logo"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbEPrCQSxt3bwTSYQV0pglF1_rSzHIxR8aFab9uRdUInfa2gxVe1DHuuz0AEn3OWAOP8s&usqp=CAU"
+            alt=""
+            style={{
+              display: "block",
+              width: "70px",
+
+              borderRadius: "100%",
+            }}
+          />
+          <a
+            className="navbar-brand"
+            to="#"
+            style={{ fontFamily: "Satisfy", color: "white" }}
+          >
             PODCASTER
           </a>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler bg-white"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -23,42 +36,62 @@ const Header = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon" ></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink
                   className="nav-link active"
                   aria-current="page"
                   to="/"
-                  style={{color:"white"}}
+                  style={{ color: "white" }}
                 >
                   Home
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/login" style={{color:"white"}}>
+                <NavLink
+                  className="nav-link"
+                  to="/login"
+                  style={{ color: "white" }}
+                >
                   Login
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/signup" style={{color:"white"}}>
+                <NavLink
+                  className="nav-link"
+                  to="/signup"
+                  style={{ color: "white" }}
+                >
                   Signup
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/addPodcast" style={{color:"white"}}>
+                <NavLink
+                  className="nav-link"
+                  to="/addPodcast"
+                  style={{ color: "white" }}
+                >
                   AddPodcast
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/listPodcast" style={{color:"white"}}>
+                <NavLink
+                  className="nav-link"
+                  to="/listPodcast"
+                  style={{ color: "white" }}
+                >
                   ListPodcast
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/view" style={{color:"white"}}>
+                <NavLink
+                  className="nav-link"
+                  to="/view"
+                  style={{ color: "white" }}
+                >
                   ViewPodcast
                 </NavLink>
               </li>
@@ -77,17 +110,17 @@ const Header = () => {
                 <a className="nav-link disabled">Disabled</a>
               </li> */}
             </ul>
-            <form className="d-flex" role="search">
+            {/* <form className="d-flex" role="search">
               <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
-              />
-              <button className="btnn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+              /> */}
+            <button className="btnn btn-outline-success" type="submit">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+            {/* </form> */}
           </div>
         </div>
       </nav>
